37c64262e6e095ac7822ae9473b1acc4d585a5ed
Yue-design <65311256+Yue-design@users.noreply.github.com>
2020-06-29 18:01:23 -0400

    Update addUserPage.js

--- frontend/src/addUserPage.js
+++ frontend/src/addUserPage.js
@@ -1,44 +1,49 @@
-<! DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- REACT LIBRARY-->
-    <script src="https://unpkg.com/react@15.5.4/dist/react.js"></script>
-    <!-- REACT DOM LIBRARY-->
-    <script src="https://unpkg.com/react-dom@15.5.4/dist/react-dom.js"></script>
-    <!-- BABEL LIBRARY-->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.25.0/babel.min.js"></script>
-    <title>addUser</title>
-</head>
-<body>
-    <!-- DESIGNATED LOCATION TO INSERT REACT CONTENT-->
-    <div id="user">fail</div>
-    <script type="text/babel"> 
-    // RENDER REACT TO THE DOM - ACCEPT TWO ARGUMENTS
-    ReactDOM.render(
-        // REACT WHAT - VARIABLE FOR JSX
-        <div>
-            <h1>Create a New User</h1>
-            <p>User Name * <input type="text" id="#" name="#"></input>
-               First Name <input type="text" id="#" name="#"></input>
-            </p>
-            <p>Email * <input type="text" id="#" name="#"></input></p>
-            <p>Password * <input type="text" id="#" name="#"></input></p>
-            <p>Permission * <input type="text" id="#" name="#"></input></p>
-            <p>Role * <input type="text" id="#" name="#"></input></p>
-            <p>First Name <input type="text" id="#" name="#"></input></p>
-            <p>Last Name <input type="text" id="#" name="#"></input></p>
-            <p>Title <input type="text" id="#" name="#"></input></p>
-            <p>Phone <input type="text" id="#" name="#"></input></p>
-            <p>Address <input type="text" id="#" name="#"></input></p>
-            <button>Cancel</button>
-            <button>Submit</button>
-        </div>,
-        // REACT WHERE - LOCATION IN THE DOM
-        document.getElementById("user")
-    );
-    </script>
-</body>
-</html>
+import React, {useState} from 'react';
+import './App.css';
+import User from './User/User.js'
+
+const app = props => {
+  const [attributesState, setAttributesState] = useState({
+    attributes: [
+      {required: 'User Name *', optional: 'First Name'},
+      {required: 'Email *', optional: 'Last Name'},
+      {required: 'Password *', optional: 'Title'},
+      {required: 'Permission *', optional: 'Phone'},
+      {required: 'Role *', optional: 'Address'}
+    ]
+  });
+
+  const [otherState, setOtherState] = useState('Invalid');
+
+  const resetHandler = (event) => {
+    setAttributesState({
+      attributes: [
+        {required: 'User Name *', optional: 'First Name'},
+        {required: 'Email *', optional: 'Last Name'},
+        {required: 'Password *', optional: 'Title'},
+        {required: 'Permission *', optional: 'Phone'},
+        {required: 'Role *', optional: 'Address'}
+      ],
+      otherState: attributesState.otherState
+    });
+  };
+
+  // const style = {
+  //   display: 'inline'
+  // };
+
+  return (
+    <div className="App">
+      <h1>Create a New User</h1>
+      <User required={attributesState.attributes[0].required} optional={attributesState.attributes[0].optional}/>
+      <User required={attributesState.attributes[1].required} optional={attributesState.attributes[1].optional}/>
+      <User required={attributesState.attributes[2].required} optional={attributesState.attributes[2].optional}/>
+      <User required={attributesState.attributes[3].required} optional={attributesState.attributes[3].optional}/>
+      <User required={attributesState.attributes[4].required} optional={attributesState.attributes[4].optional}/>
+      <button onClick={resetHandler}>Cancel</button>
+      <button onClick={resetHandler}>Submit</button>
+    </div>
+  );
+}
+
+export default app;
